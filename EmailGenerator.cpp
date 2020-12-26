f310bc25b047f2009d0160e9b57aee8800671d75
BingQuan <bingquan6102@gmail.com>
2020-12-26 23:23:41 +0800

    Generate random email success
    Able to generate random email address

--- EmailGenerator.cpp
+++ EmailGenerator.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-char randomGenerator(int choice) {
-    // 1:[A-Za-z0-9], 2:[A-Za-z]
-    int r;
-    srand(time(NULL));
-    switch(choice) {
-        case 1: r = rand()%3+1;
-                switch(r) {
-                    case 1: return char(rand()%26+65);
-                    case 2: return char(rand()%26+97);
-                    case 3: return char(rand()%10+48);
-                }
-        case 2: r = rand()%2+1;
-                switch(r) {
-                    case 1: return char(rand()%26+65);
-                    case 2: return char(rand()%26+97);
-                }
-        default: cout << "Forgot to enter a choice!" << endl;
-                 return '~';
-    }
-}
+char all[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+string charGenerator(int l);
+string alphaGenerator(int l);
+string domainGenerator();
 
 int main() {
     // [A-Za-z0-9]{4}\.[A-Za-z0-9]{5}@[A-Za-z]{4}\.(com|my|org)
-    
-    
+    string email;
+    email += charGenerator(4);    
+    email += ".";
+    email += charGenerator(5);  
+    email += "@";
+    email += alphaGenerator(4);
+    email += ".";
+    email += domainGenerator();
+    cout << email << endl;
+    return 0;
+}
+
+string charGenerator(int l) {
+    string ans;
+    for (int i = 0; i < l; i++) {
+        ans += all[rand()%62];
+    }
+    return ans;
+}
+
+string alphaGenerator(int l) {
+    string ans;
+    for (int i = 0; i < l; i++) {
+        ans += alphabet[rand()%52];
+    }
+    return ans;
+}
+
+string domainGenerator() {
+    string domain[] = {"com", "my", "org"};
+    return domain[rand()%2];
 }
