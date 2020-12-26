a2b1b038cc04b88761b17efd7be4bd1dfee3a9e6
Ivan Yap <ivanyap0612@gmail.com>
2020-12-27 03:05:31 +0800

    check for duplicates, updated code with some minor changes
    check for duplicates, updated code with some minor changes

--- EmailGenerator.cpp
+++ EmailGenerator.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 char all[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -9,11 +12,31 @@ char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 string charGenerator(int l);
 string alphaGenerator(int l);
 string domainGenerator();
+bool duplicateValidate(string str, vector<string> &vect);
 
 int main() {
+    vector<string> vect;
     // [A-Za-z0-9]{4}\.[A-Za-z0-9]{5}@[A-Za-z]{4}\.(com|my|org)
-        for (int i = 0; i < 100; i++) {
-        cout << charGenerator(4) + "." + charGenerator(5) + "@" + alphaGenerator(4) + "." + domainGenerator() << endl;
+    string email;
+    int count = 1;
+    while(count <= 10000){
+        email ="";
+        email += charGenerator(4);
+        email += ".";
+        email += charGenerator(5);
+        email += "@";
+        email += alphaGenerator(4);
+        email += ".";
+        email += domainGenerator();
+        if (duplicateValidate(email,vect)){
+            vect.push_back(email);
+            count++;
+        }
+        else{}
+        }
+
+    for(int i = 0; i < vect.size(); i++){
+        cout << vect[i] << endl;
     }
     return 0;
 }
@@ -37,4 +60,11 @@ string alphaGenerator(int l) {
 string domainGenerator() {
     string domain[] = {"com", "my", "org"};
     return domain[rand()%3];
+}
+
+bool duplicateValidate(string str, vector<string> &vect){
+    if(find(vect.begin(), vect.end(), str) != vect.end())
+        return false;
+    else
+        return true;
 }
