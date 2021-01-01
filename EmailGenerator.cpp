d6242d644eb1ab1ee140066bafb8e4525b07b2fb
BingQuan <bingquan6102@gmail.com>
2021-01-01 23:47:12 +0800

    Reused vector for storing emails
    - created a folder for putting data sets
    - reused vector in EmailGenerator
    - EmailGenerator generates email addresses without the last "\n"
    - removed last "\n" in each data sets

--- EmailGenerator.cpp
+++ EmailGenerator.cpp
@@ -12,35 +12,34 @@ static char all[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456
 static char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 void generateEmail(string filename, int num);
-bool searchDuplicate(string array[], int start, int end, string target);
-void insertSorted(string array[], int n, string key);
 string charGenerator(int l);
 string alphaGenerator(int l);
 string domainGenerator();
-// bool duplicateValidate(string str, vector<string> &vect);
+bool duplicateValidate(string &str, vector<string> &vect);
 
 int main() {
     srand(time(NULL));
-    generateEmail("setA", 10);
-    // generateEmail("setB", 100000);
-    // generateEmail("setC", 500000); 
+    generateEmail("Data Set A", 100);
+    // generateEmail("Data Set B", 100000);
+    // generateEmail("Data Set C", 500000); 
+
     return 0;
 }
 
 // generates email data, also generates a .txt file for reference
 void generateEmail(string filename, int num) {
     ofstream file; 
-    file.open(filename + ".txt");
+    file.open("datasets/" + filename + ".txt");
 
-    // array of string
-    string emailSet[num] = "0";
+    // declare a string vector
+    vector<string> vect;
     // email format: [A-Za-z0-9]{4}\.[A-Za-z0-9]{5}@[A-Za-z]{4}\.(com|my|org)
     string email;
     int count = 1;
     auto start = chrono::system_clock::now();
     // Generate specified number of email addresses
     while(count <= num){
-        email ="";
+        email = "";
         email += charGenerator(4);
         email += ".";
         email += charGenerator(5);
@@ -48,21 +47,18 @@ void generateEmail(string filename, int num) {
         email += alphaGenerator(4);
         email += ".";
         email += domainGenerator();
-        cout << email << endl;
-        // if no duplicate string is found, add string to the array
-        if (!searchDuplicate(emailSet, 0, count-1, email)){ // check duplicates
-            cout << "no duplicate.." << endl;
-            insertSorted(emailSet, count-1, email);
-            cout << "email inserted" << endl;
+        // if no duplicate string is found, add string to the vector
+        if (duplicateValidate(email,vect)){
+            if(count != 1) {
+                file << endl;
+            }
+            vect.push_back(email);
+            file << email;
             count++;
         }
-        else {
-            cout << "duplicated" << endl;
+        else{
+            continue; // data not recorded
         }
-    }
-
-    for (string &email : emailSet) {
-        file << email << endl;
     }
     
     auto end = chrono::system_clock::now();
@@ -71,38 +67,6 @@ void generateEmail(string filename, int num) {
     // close file
     file.close();
 
-}
-
-// search for duplicates
-// modified binary search for strings
-bool searchDuplicate(string array[], int start, int end, string target) {
-    if (end == 0) {
-        return false; // empty array
-    }
-    while (start <= end) {
-        int middle = start+(end-1)/2;
-
-        if (target.compare(array[middle]) == 0) {
-            return true; // string is found, duplicate exists
-        }
-
-        if (target.compare(array[middle]) > 0)  {
-            start = middle + 1;
-        }
-        else {
-            end = middle - 1;
-        }
-    }
-    return false; // string not found, duplicate does not exists
-}
-
-// insert into sorted array
-void insertSorted(string array[], int n, string key) {
-    int i;
-    for (i = n-1; i >= 0 && key.compare(array[i]) < 0; i--) {
-        array[i+1] = array[i];
-    }
-    array[i+1] = key;
 }
 
 string charGenerator(int l) {
@@ -126,12 +90,10 @@ string domainGenerator() {
     return domain[rand()%3];
 }
 
-/*
-bool duplicateValidate(string str, vector<string> &vect){
+bool duplicateValidate(string &str, vector<string> &vect){
     // return false if duplicate string is found
     if(find(vect.begin(), vect.end(), str) != vect.end())
         return false;
     else
         return true;
 }
-*/
