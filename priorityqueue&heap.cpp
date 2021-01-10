e8af977b0833251e7cb7e1221ab3d35101ff32ef
Ivan Yap <ivanyap0612@gmail.com>
2021-01-10 16:59:26 +0800

    Update priorityqueue&heap.cpp

--- priorityqueue&heap.cpp
+++ priorityqueue&heap.cpp
@@ -179,7 +179,7 @@ int main()
     cout << "***************************************" << endl;
     cout << "Priority Queue and Heap Implementation" << endl;
     cout << "***************************************" << endl;
-    cout << "Please select one of the datasets below:\n1.Dataset A(100 items)\n2.Dataset B(100,000 items)\n3.Dataset C(500,000 items)\n4.Exit" << endl;
+    cout << "Please select one of the datasets below:\n1.Data Set A(100 items)\n2.Data Set B(100,000 items)\n3.Data Set C(500,000 items)\n4.Exit" << endl;
     cout << "Enter your choice: ";
     // Prompt user to enter their choice
     cin >> choice;
