141bdad51ba7cfe62e3bd6d0ef20976d18d82749
Ivan Yap <ivanyap0612@gmail.com>
2021-02-15 14:33:20 +0800

    Update Program2 - Removed Global Variable
    Removed Global Variable - Added constant,etc

--- algo-design-2/program2_dijkstra_algorithm.cpp
+++ algo-design-2/program2_dijkstra_algorithm.cpp
@@ -7,29 +7,29 @@
 
 using namespace std;
 
-static char characters[] = "ABCDEFGHIJ";
-double matrix[10][10];   // adjacency matrix of planets
-int numVertex = 10; // declare the number of vertex
-void getDistance(); // read planet distances from the text file
-void initAdjancencyMatrix(); // initialize the map
-void addEdge(int a, int b, double value); // (for adjacency matrix) adds a new edge in the map
-void printAdjacencyMatrix(); // display the adjacency matrix
+const int NUM_VERTEX = 10; // declare the number of vertex
+
+void getDistance(double matrix[][NUM_VERTEX]); // read planet distances from the text file
+void initAdjancencyMatrix(double matrix[][NUM_VERTEX]); // initialize the map
+void addEdge(double matrix[][NUM_VERTEX],int a, int b, double value); // (for adjacency matrix) adds a new edge in the map
+void printAdjacencyMatrix(double matrix[][NUM_VERTEX]); // display the adjacency matrix
 void printShortestDistance(double distance[]); // print the constructed distance array
 void minDistance(double distance[],bool sptSet[]); // find the vertex with minimum distance value
-void dijkstra(int source); //function that implements Dijkstra's algorithm by using adjacency matrix representation
+void dijkstra(double matrix[][NUM_VERTEX],int source); //function that implements Dijkstra's algorithm by using adjacency matrix representation
 
 int main() {
 
-    initAdjancencyMatrix();
-    getDistance();
-    printAdjacencyMatrix();
-    dijkstra(0);
+    double matrix[10][10];   // adjacency matrix of planets
+    initAdjancencyMatrix(matrix);
+    getDistance(matrix);
+    printAdjacencyMatrix(matrix);
+    dijkstra(matrix,0);
+
     return 0;
 }
 
 // read planet distances from the text file
-void getDistance() {
-
+void getDistance(double matrix[][NUM_VERTEX]) {
     string details;
     ifstream file("generated-data/planet-distances.txt");
     for (int i = 0; i < 10; i++) {
@@ -42,36 +42,36 @@ void getDistance() {
 }
 
 // initialize the map
-void initAdjancencyMatrix() {
-
-    addEdge(0, 3, 1); // AD
-    addEdge(0, 5, 1); // AF
-    addEdge(0, 7, 1); // AH
-    addEdge(0, 9, 1); // AJ
-    addEdge(3, 9, 1); // DJ
-    addEdge(7, 9, 1); // HJ
-    addEdge(5, 7, 1); // FH
-    addEdge(1, 3, 1); // BD
-    addEdge(6, 9, 1); // GJ
-    addEdge(7, 8, 1); // HI
-    addEdge(2, 5, 1); // CF
-    addEdge(1, 6, 1); // BG
-    addEdge(6, 8, 1); // GI
-    addEdge(2, 8, 1); // CI
-    addEdge(1, 4, 1); // BE
-    addEdge(4, 6, 1); // EG
-    addEdge(4, 8, 1); // EI
-    addEdge(2, 4, 1); // CE
+void initAdjancencyMatrix(double matrix[][NUM_VERTEX]) {
+    addEdge(matrix, 0, 3, 1); // AD
+    addEdge(matrix, 0, 5, 1); // AF
+    addEdge(matrix, 0, 7, 1); // AH
+    addEdge(matrix, 0, 9, 1); // AJ
+    addEdge(matrix, 3, 9, 1); // DJ
+    addEdge(matrix, 7, 9, 1); // HJ
+    addEdge(matrix, 5, 7, 1); // FH
+    addEdge(matrix, 1, 3, 1); // BD
+    addEdge(matrix, 6, 9, 1); // GJ
+    addEdge(matrix, 7, 8, 1); // HI
+    addEdge(matrix, 2, 5, 1); // CF
+    addEdge(matrix, 1, 6, 1); // BG
+    addEdge(matrix, 6, 8, 1); // GI
+    addEdge(matrix, 2, 8, 1); // CI
+    addEdge(matrix, 1, 4, 1); // BE
+    addEdge(matrix, 4, 6, 1); // EG
+    addEdge(matrix, 4, 8, 1); // EI
+    addEdge(matrix, 2, 4, 1); // CE
 }
 
 // (for adjacency matrix) adds a new edge in the map
-void addEdge(int a, int b, double value) {
+void addEdge(double matrix[][NUM_VERTEX],int a, int b, double value) {
     matrix[a][b] = value;
     matrix[b][a] = value;
 }
 
 // print adjacency matrix
-void printAdjacencyMatrix() {
+void printAdjacencyMatrix(double matrix[][NUM_VERTEX]) {
+    char characters[] = "ABCDEFGHIJ";
     cout << "Adjacency Matrix:" << endl;
     cout << " ";
     for (int i = 0; i < 10; i++) {
@@ -90,9 +90,10 @@ void printAdjacencyMatrix() {
 
 // print the constructed distance array
 void printShortestDistance(double distance[]){
+    char characters[] = "ABCDEFGHIJ";
     cout << "\n\nThe shortest distance to each planet:\n";
     cout<<"Vertex\t\tDistance from source"<<endl;
-	for(int i=0;i<numVertex;i++){
+	for(int i=0;i<NUM_VERTEX;i++){
 		//char c=65+i;
 		cout<<characters[i]<<"\t\t"<<distance[i]<<endl;
 	}
@@ -101,7 +102,7 @@ void printShortestDistance(double distance[]){
 // find the vertex with minimum distance value
 int minimumDistance(double distance[],bool sptSet[]){
     int minDistance = INT_MAX,minIndex;
-    for(int i = 0;i<numVertex;i++){
+    for(int i = 0;i<NUM_VERTEX;i++){
         if(sptSet[i]==false){
             if(distance[i]<=minDistance)
             {
@@ -114,13 +115,13 @@ int minimumDistance(double distance[],bool sptSet[]){
 }
 
 //function that implements Dijkstra's algorithm by using adjacency matrix representation
-void dijkstra(int source){
+void dijkstra(double matrix[][NUM_VERTEX],int source){
+    char characters[] = "ABCDEFGHIJ";
+    int path[NUM_VERTEX];
+    double distance[NUM_VERTEX];
+    bool sptSet[NUM_VERTEX];
 
-    int path[numVertex];
-    double distance[numVertex];
-    bool sptSet[numVertex];
-
-    for (int i = 0; i < numVertex; i++){
+    for (int i = 0; i < NUM_VERTEX; i++){
         // Initialize the variables
         distance[i] = INT_MAX;
         sptSet[i] = false;
@@ -129,13 +130,13 @@ void dijkstra(int source){
     //initialize the source vertex distance to zero
     distance[source] = 0;
 
-    for (int i = 0; i < numVertex; i++) {
+    for (int i = 0; i < NUM_VERTEX; i++) {
 
         int u = minimumDistance(distance, sptSet);
         // Mark the picked vertex as visited
         sptSet[u] = true;
 
-        for (int v = 0; v < numVertex; v++){
+        for (int v = 0; v < NUM_VERTEX; v++){
             // update the minimum distance
             if (!sptSet[v] && matrix[u][v] && distance[u] != INT_MAX
                 && distance[u] + matrix[u][v] < distance[v]){
@@ -147,7 +148,7 @@ void dijkstra(int source){
     }
 
     cout << "The shortest paths from Planet A to the other planets:";
-    for(int i = 0;i < numVertex;i++){
+    for(int i = 0;i < NUM_VERTEX;i++){
         if(i!=source) {
           int j = 0;
           cout<<"\nPath = "<<characters[i];
